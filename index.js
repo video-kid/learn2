98797fe3bdfa2173f78061f951abb894f54f7cb0
Wioleta Tyranska <wioleta.tyranska@gmail.com>
2018-01-17 20:52:13 +0100

    i'm just plaing with jquerry a little ro make a real thing in future

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 $(document).ready(function()
 {
-    let stickyMenu = $('#bookingSite #menu');
+    /* let stickyMenu = $('#bookingSite #menu');
     
 
     let majonez = function() {
         stickyMenu.toggleClass('sticky');
     };
 
-    
-    stickyMenu.click(majonez);
-});
+    stickyMenu.click(majonez); */
 
+    let letItSticky = function(selector) {
+        let element = $(selector);
+
+        let toggleHandler = function() {
+            element.toggleClass('sticky');
+        }
+
+        element.click(toggleHandler);
+    }
+
+    letItSticky('#bookingSite #menu');
+    letItSticky('footer');
+});
